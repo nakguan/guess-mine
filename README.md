9a91e1f26a96673a49518298112a29af75531835
nakguan <nakguan@gmail.com>
2019-12-11 00:46:18 +0900

    환경 설정 완료 (1.4 Setting Up SocketIO)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # guess-mine
+
 Realtime Game built with SocketIO, Gulp and Node
+
+## To Do:
+
+- [ ] Server
+- [ ] Pug
+- [ ] Static
+- [ ] Socket IO
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
+
+const PORT = 8080;
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const handleListening = () =>
+  console.log(`✔️ Sever running: http://localhost:${PORT}`);
+
+const server = app.listen(PORT, handleListening);
+const io = socketIO(server);
