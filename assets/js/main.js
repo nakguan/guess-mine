1011baf7f112f748dd538e71c5f8f921ad8424f6
nakguan <nakguan@gmail.com>
2019-12-12 13:38:48 +0900

    gulp 설정 ~3.3 Browserify part Two

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import { handleMessageNotif } from "./chat";
+
 const socket = io("/");
 
 const sendMessage = message => {
@@ -9,11 +11,6 @@ const sendMessage = message => {
 
 const setNickname = nickname => {
   socket.emit("setNickname", { nickname });
-};
-
-const handleMessageNotif = data => {
-  const { message, nickname } = data;
-  console.log(`${nickname} : ${message}`);
 };
 
 socket.on("messageNoti", handleMessageNotif);
---
+++ gulpfile.babel.js
@@ -0,0 +1,63 @@
+import gulp from "gulp";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
+import babel from "babelify";
+
+const paths = {
+  styles: {
+    src: "assets/scss/styles.scss",
+    dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss"
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js"
+  }
+};
+
+const clean = () => {
+  return del("src/static");
+};
+
+const styles = () => {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babel.configure({
+            presets: ["@babel/preset-env"]
+          })
+        ]
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+const watchFiles = () => {
+  gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.js.watch, js);
+};
+
+const dev = gulp.series([clean, styles, js, watchFiles]);
+
+export const build = gulp.series(clean, styles, js);
+
+export default dev;
